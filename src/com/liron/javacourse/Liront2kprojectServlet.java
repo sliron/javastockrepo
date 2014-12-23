869ec781cc56ad0e3cda66477afbe0bea421008e
sliron <liron.schneiderman@gmail.com>
2014-12-23 20:02:49 +0200

    exercise 02 - Math

--- src/com/liron/javacourse/Liront2kprojectServlet.java
+++ src/com/liron/javacourse/Liront2kprojectServlet.java
@@ -5,14 +5,18 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Liront2kprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html");
 		
 		int num1, num2, num3, result;
 		num1=4;
 		num2=3;
 		num3=7;
 		result= (num1+num2) * num3;
-		resp.getWriter().println("Result of (num1+num2) * num3 = 49");
+		
+		String resultStr = new String("<h1>Result of "+"("+num1+"+"+num2+")"+"*"+num3+"="+result+"</h1>");
+		
+		resp.getWriter().println(resultStr);
+		
 		
 	}
 }
